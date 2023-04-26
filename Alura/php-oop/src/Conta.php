39cdc81506cf2fa56a7aa724dcfce9b000208677
Kelly Castelo <kawokkun@gmail.com>
2023-04-26 12:28:20 -0300

    metodos por referencia

--- Alura/php-oop/src/Conta.php
+++ Alura/php-oop/src/Conta.php
@@ -4,20 +4,42 @@ class Conta
 {
     private $cpfTitular;
     private $nomeTitular;
-    private $saldo;
+    private $saldo = 0.0;
 
-    public function __construct($cpfTitular, $nomeTitular, $saldo)
+    public function __construct($cpfTitular, $nomeTitular)
     {
         $this->cpfTitular = $cpfTitular;
         $this->nomeTitular = $nomeTitular;
-        $this->saldo = $saldo;
     }
 
-    public function sacar($valorASacar) {
+    public function sacar($valorASacar)
+    {
         if($valorASacar < 0 || $valorASacar > $this->saldo || $this->saldo - $valorASacar < 0) {
-            echo 'saldo indisponível';
+            return false;
         } else {
             $this->saldo -= $valorASacar;
+            return true;
         }
+    }
+
+    public function depositar($valorADepositar)
+    {
+        if($valorADepositar < 0 ) {
+            return false;
+        } else {
+            $this->saldo += $valorADepositar;
+            return true;
+        }
+    }
+
+    public function transferencia(Conta $conta, $valorATransferir)
+    {
+        if($valorATransferir >= 0) {
+            if ($this->sacar($valorATransferir)) {
+                $conta->depositar($valorATransferir);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Alura/php-oop/teste.php
+++ Alura/php-oop/teste.php
@@ -2,8 +2,15 @@
 
 require_once('./src/Conta.php');
 
-$conta = new Conta("12345678910", "Castelo", 200);
+$conta = new Conta("12345678910", "Castelo");
 
+$conta->depositar(800);
 $conta->sacar(300);
 
+$contaLu = new Conta("9999999999", "Lucas");
+
+$contaLu->depositar(800);
+$contaLu->transferencia($conta, 900.0);
+
 echo var_dump($conta);
+echo var_dump($contaLu);
