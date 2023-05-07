ea56f24d925a7413359f0265219d374854dd080f
Kelly Castelo <kawokkun@gmail.com>
2023-05-07 06:42:55 -0300

    transacoes com pdo

---
+++ learning-pdo/criar-turma.php
@@ -0,0 +1,20 @@
+<?php
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infraestructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infraestructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+//begin transaction
+$connection->beginTransaction();
+
+$studentRepository->save(new Student(null, "Coral Coraline", new DateTimeImmutable('2012-07-03')));
+$studentRepository->save(new Student(null, "Cassiel Wolfe Hughes", new DateTimeImmutable('2012-10-25')));
+$studentRepository->save(new Student(null, "Lil Llus", new DateTimeImmutable('1985-12-01')));
+
+$connection->commit(); //finaliza
+
+//se precisar cancelar a transacao use rollback
