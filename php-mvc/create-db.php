664774aaa313ebfe7af0ee38b027bab0b91731ef
Kelly Castelo <kawokkun@gmail.com>
2023-05-08 07:58:57 -0300

    criando tabela users

--- php-mvc/create-db.php
+++ php-mvc/create-db.php
@@ -1,5 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 use Zam0k\PhpMvc\config\Connection;
 
 $pdo = Connection::create();
-$pdo->exec('CREATE TABLE videos (id INTEGER PRIMARY KEY, url TEXT, title TEXT)');
+//$pdo->exec('CREATE TABLE videos (id INTEGER PRIMARY KEY, url TEXT, title TEXT)');
+
+$pdo->exec('CREATE TABLE users (id INTEGER PRIMARY KEY, email TEXT, password TEXT);');
